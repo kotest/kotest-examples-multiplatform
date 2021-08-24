d1a420c1bf63fce5c73c23791ba2377c5776683a
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-08-24 20:31:26 +0200

    Add 3 test files

---
+++ src/commonTest/kotlin/io/kotest/examples/mpp/TestThree.kt
@@ -0,0 +1,86 @@
+package io.kotest.examples.mpp
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
+
+class TestThree : StringSpec({
+   "This test is skipped" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped2" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped3" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped4" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped5" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped6" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped7" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped8" {
+      delay(10)
+      true shouldBe true
+   }
+   "This test is skipped9" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped10" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped11" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped12" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped13" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped14" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped15" {
+      delay(10)
+      true shouldBe true
+   }
+
+   "This test is skipped16" {
+      delay(10)
+      true shouldBe true
+   }
+})
